b616c6b255fec7f3aedf249537ee8ed9b28c5399
frank-zsy <syzhao1988@126.com>
2024-08-26 23:36:13 +0800

    blog: ospp 2023 analysis
    Signed-off-by: frank-zsy <syzhao1988@126.com>

---
+++ src/components/SimpleTable/index.tsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import { BoardContainer } from '../Leaderboard/BoardContainer';
+import { BaseBoard } from '../Leaderboard/BaseBoard';
+import { useReactTable, createColumnHelper, getCoreRowModel } from '@tanstack/react-table';
+import { COLUMN_TYPE_RULES } from '../Leaderboard';
+
+const helper = createColumnHelper<{ __index__: number }>();
+
+export default (props: { title: string; data: any[]; options: any[] }): JSX.Element => {
+
+  console.log(props);
+
+  const tableColumns = [helper.accessor(row => row, {
+    id: `__index__`,
+    header: () => '#',
+    cell: info => <span>{info.getValue().__index__}</span>,
+    size: 30,
+  })];
+  tableColumns.push(...props.options.map((option, index) => {
+    const { name, width, type, fields } = option;
+    const column = helper.accessor(row => row, {
+      id: `${index}-${name}`,
+      header: () => name,
+      cell: info => {
+        const columnTypeRule = COLUMN_TYPE_RULES.find(rule => rule.name === type);
+        const renderProps = fields.map(field => info.getValue()[field]);
+        return columnTypeRule.renderer(...renderProps);
+      },
+      size: width,
+    });
+    return column;
+  }));
+
+  const table = useReactTable({
+    columns: tableColumns,
+    data: props.data,
+    getCoreRowModel: getCoreRowModel()
+  });
+
+  return (
+    <BoardContainer title={props.title}>
+      <BaseBoard table={table} />
+    </BoardContainer>
+  );
+}
