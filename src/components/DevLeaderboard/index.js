d7991f96331120729a83ae6bef025b4d0b00ef51
Bruce-Jay <yirenlin0218@gmail.com>
2024-09-05 18:30:54 +0800

    feat: 初步完成排行榜 banner 样式实现
    Signed-off-by: Bruce-Jay <yirenlin0218@gmail.com>

--- src/components/DevLeaderboard/index.js
+++ src/components/DevLeaderboard/index.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import * as echarts from "echarts";
 import $ from "jquery";
+import { Select, Input, Button, Space, Calendar } from "antd";
 
 import Leaderboard from "./Leaderboard";
 import Details from "./Details";
@@ -8,60 +9,83 @@ import Graph from "./Graph";
 import "./devLeaderboard.css";
 
 const DevLeaderboard = () => {
-  const [graph, setGraph] = useState(null);
-  const [selectedNodeId, setSelectedNodeId] = useState(null);
+	const [graph, setGraph] = useState(null);
+	const [selectedNodeId, setSelectedNodeId] = useState(null);
 
-  const baseUrl = "https://oss.x-lab.info/open_digger/";
-  const platform = "github"; // Replace with dynamic fetching if needed
-  const repoName = "hypertrons/hypertrons-crx"; // Replace with dynamic fetching if needed
-  const month = "202407"; // Replace with dynamic fetching if needed
-  const typeMap = new Map([
-    ["r", "repo"],
-    ["i", "issue"],
-    ["p", "pull"],
-    ["u", "user"],
-  ]);
+  const { Search } = Input;
 
-  useEffect(() => {
-    const loadData = async () => {
-      const data = await $.getJSON(
-        `${baseUrl}${platform}/${repoName}/community_openrank.json`
-      );
-      setGraph(data);
-    };
+	const baseUrl = "https://oss.x-lab.info/open_digger/";
+	const platform = "github"; // Replace with dynamic fetching if needed
+	const repoName = "hypertrons/hypertrons-crx"; // Replace with dynamic fetching if needed
+	const month = "202407"; // Replace with dynamic fetching if needed
+	const typeMap = new Map([
+		["r", "repo"],
+		["i", "issue"],
+		["p", "pull"],
+		["u", "user"],
+	]);
 
-    loadData();
-  }, [platform, repoName]);
+	useEffect(() => {
+		const loadData = async () => {
+			const data = await $.getJSON(
+				`${baseUrl}${platform}/${repoName}/community_openrank.json`
+			);
+			setGraph(data);
+		};
 
-  const handleNodeDblClick = (id) => {
-    setSelectedNodeId(id);
-  };
+		loadData();
+	}, [platform, repoName]);
 
-  return (
-    <div id="main" className="bordered">
-      <Graph
-        graph={graph}
-        month={month}
-        repoName={repoName}
-        platform={platform}
-        typeMap={typeMap}
-        onNodeDblClick={handleNodeDblClick}
-      />
-      <div
-        id="control"
-        className="bordered"
-      >
-        <Leaderboard graph={graph} month={month} />
-        <Details
-          graph={graph}
-          id={selectedNodeId}
-          month={month}
-          typeMap={typeMap}
-          platform={platform}
-        />
-      </div>
-    </div>
-  );
+	const handleNodeDblClick = (id) => {
+		setSelectedNodeId(id);
+	};
+
+	return (
+		<>
+			<div className="banner-box">
+				<div className="banner-title">OpenRank贡献度排行榜</div>
+				<div className="banner-content">
+					<Select
+						defaultValue="GitHub"
+						style={{ width: 120 }}
+						options={[
+							{ value: "GitHub", label: "GitHub" },
+							{ value: "Gitee", label: "Gitee" },
+							{ value: "GitLab", label: "GitLab" },
+						]}
+					/>
+          <div>
+            <Space.Compact style={{ width: "300px" }}>
+              <Input defaultValue="Combine input and button" />
+              <Button type="primary">Submit</Button>
+            </Space.Compact>
+          </div>
+					
+          <Calendar fullscreen={false}/>
+				</div>
+			</div>
+			<div id="main" className="bordered">
+				<Graph
+					graph={graph}
+					month={month}
+					repoName={repoName}
+					platform={platform}
+					typeMap={typeMap}
+					onNodeDblClick={handleNodeDblClick}
+				/>
+				<div id="control" className="bordered">
+					<Leaderboard graph={graph} month={month} />
+					<Details
+						graph={graph}
+						id={selectedNodeId}
+						month={month}
+						typeMap={typeMap}
+						platform={platform}
+					/>
+				</div>
+			</div>
+		</>
+	);
 };
 
 export default DevLeaderboard;
