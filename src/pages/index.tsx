77b9623a18cba3bf0685756119fa8a27b69cca99
frank-zsy <syzhao1988@126.com>
2024-10-13 17:59:37 +0800

    feat: add intro audio to homepage
    Signed-off-by: frank-zsy <syzhao1988@126.com>

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,24 +1,34 @@
+import React, { useRef } from 'react';
 import clsx from 'clsx';
-import Link from '@docusaurus/Link';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import HomepageFeatures from '../components/HomepageFeatures';
 import Layout from '@theme/Layout';
 import Translate, { translate } from '@docusaurus/Translate';
 
 import styles from './index.module.css';
+import Link from '@docusaurus/Link';
 
 function HomepageHeader() {
+  const audioRef = useRef<HTMLAudioElement>(null);
+
+  const handleAudioPlay = () => {
+    if (audioRef.current) {
+      audioRef.current.play();
+    }
+  };
+
   return (
     <header className={clsx('hero hero--primary', styles.heroBanner)}>
       <div className="container">
         <img src="img/logo/logo-black-blue-combination-horizontal.png" alt="Logo" /> {/* 插入图片 */}
         <p className="hero__subtitle">{translate({ id: 'homepage.tagLine' })}</p>
-        <div className={styles.buttons}>
+        <div className={styles.buttons} onClick={handleAudioPlay}>
           <Link
             className="button button--secondary button--lg"
-            to="/docs/user_docs/intro">
+            onClick={handleAudioPlay}>
             <Translate id="homepage.getStarted" />
           </Link>
+          <audio ref={audioRef} src="audio/intro_en.mp3" />
         </div>
       </div>
     </header>
