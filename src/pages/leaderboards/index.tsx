7925082548f1282a9dc45b13272755b3871b553f
frank-zsy <syzhao1988@126.com>
2024-12-02 02:41:33 +0800

    support division leaderboard
    Signed-off-by: frank-zsy <syzhao1988@126.com>

--- src/pages/leaderboards/index.tsx
+++ src/pages/leaderboards/index.tsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react';
 import Layout from '@theme/Layout';
 import SimpleTable from '../../components/SimpleTable';
 import { translate } from '@docusaurus/Translate';
+import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import axios from 'axios';
 import styles from './styles.module.css';
 
@@ -10,6 +11,7 @@ function OpenLeaderboard() {
   const [title, setTitle] = useState('Ranking List Title');
   const [data, setData] = useState([]);
   const [options, setOptions] = useState([]);
+  const { i18n } = useDocusaurusContext();
 
   useEffect(() => {
     const search = new URLSearchParams(window.location.search);
@@ -44,7 +46,22 @@ function OpenLeaderboard() {
           { name: 'OpenRank', type: 'String', fields: ['openrank'], width: 200 },
           { name: translate({ id: 'leaderboards.companies.repos' }), type: 'String', fields: ['repoCount'], width: 200 },
           { name: translate({ id: 'leaderboards.companies.developers' }), type: 'String', fields: ['developerCount'], width: 200 },
-          { name: translate({ id: 'leaderboards.companies.country' }), type: 'String', fields: ['country'], width: 200 },
+          { name: translate({ id: 'leaderboards.companies.country' }), type: 'String', fields: [i18n.currentLocale === 'zh' ? 'country_zh' : 'country'], width: 200 },
+        ]);
+      }).catch(e => {
+        console.log(e);
+      });
+    } else if (type === 'divisions') {
+      axios.get(`https://oss.open-digger.cn/leaderboards/${type}.json`).then(resp => {
+        const data = resp.data;
+        setTitle(translate({ id: 'leaderboards.provinces.title' }));
+        setData(data.data);
+        setOptions([
+          { name: '#', type: 'String', fields: ['rank'], width: 80 },
+          { name: translate({ id: 'leaderboards.provinces.name' }), type: 'String', fields: [i18n.currentLocale === 'zh' ? 'province_zh' : 'province'], width: 300 },
+          { name: 'OpenRank', type: 'String', fields: ['openrank'], width: 300 },
+          { name: translate({ id: 'leaderboards.provinces.developers' }), type: 'String', fields: ['developerCount'], width: 300 },
+          { name: translate({ id: 'leaderboards.provinces.country' }), type: 'String', fields: [i18n.currentLocale === 'zh' ? 'country_zh' : 'country'], width: 300 },
         ]);
       }).catch(e => {
         console.log(e);
