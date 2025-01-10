6602f13252b66f3ffce94d2b012e2be712c1cd0d
frank-zsy <syzhao1988@126.com>
2025-01-10 14:44:10 +0800

    refactor: show all blog in blog sidebar
    Signed-off-by: frank-zsy <syzhao1988@126.com>

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -56,6 +56,7 @@ const config: Config = {
         blog: {
           showReadingTime: true,
           blogSidebarTitle: 'Recent Posts',
+          blogSidebarCount: 'ALL',
           remarkPlugins: [remarkMath],
           rehypePlugins: [rehypeKatex],
         },
