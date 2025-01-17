64e79638c79ef4c004d4e0057994ff2250b8f6aa
frank-zsy <syzhao1988@126.com>
2025-01-17 11:39:28 +0800

    feat: add petercat
    Signed-off-by: frank-zsy <syzhao1988@126.com>

--- src/theme/Layout/index.tsx
+++ src/theme/Layout/index.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import Layout from '@theme-original/Layout';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import { translate } from '@docusaurus/Translate';
+import { Assistant } from '@petercatai/assistant';
+import '@petercatai/assistant/style';
 
 export default function LayoutWrapper(props: any): JSX.Element {
   const { siteConfig } = useDocusaurusContext();
@@ -22,6 +24,14 @@ export default function LayoutWrapper(props: any): JSX.Element {
         )
       }
       <Layout {...props} />
+      <Assistant
+        token="7de1923b-00ad-4ea9-935b-e8d2eb5c6ecc"
+        apiDomain="https://api.petercat.ai"
+        assistantMeta={{
+          avatar: 'https://open-digger.cn/img/logo/logo-black-blue.png',
+          title: 'OpenDigger Assistant'
+        }}
+        helloMessage='我是 OpenDigger 问答小助手，很高兴跟你聊天~' />
     </>
   );
 }
