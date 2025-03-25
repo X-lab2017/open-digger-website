32b9e5d8cc61b2c41e66a5b2256233c59d8d013b
frank-zsy <syzhao1988@126.com>
2025-03-25 15:58:29 +0800

    fix: fix leaderboards url
    Signed-off-by: frank-zsy <syzhao1988@126.com>

--- src/pages/CommunityOpenRankLeaderboards/index.tsx
+++ src/pages/CommunityOpenRankLeaderboards/index.tsx
@@ -6,8 +6,10 @@ export default function CommunityOpenRankLeaderboards({ location }): JSX.Element
   const pathes = currentPath.split('/');
   const param = pathes[pathes.length - 1];
   return (
-    <Layout>
-      <iframe className={styles.mainRankingList} src='https://tyn1998.github.io/ant-openrank-leaderboard/?project=antd&timeUnit=month&timeValue=1739635200000'></iframe>
-    </Layout>
+    <div className='no-footer'>
+      <Layout>
+        <iframe className={styles.mainRankingList} src='https://tyn1998.github.io/community-openrank-leaderboard/?organization=antgroup&project=antd&timeUnit=month&timeValue=1739635200000'></iframe>
+      </Layout>
+    </div>
   );
 }
