731f7d1f1ec65378796df06a7fcac6f7f73f40ef
frank-zsy <syzhao1988@126.com>
2025-03-31 13:27:10 +0800

    refactor: update community openrank leaderboards page
    Signed-off-by: frank-zsy <syzhao1988@126.com>

--- src/pages/CommunityOpenRankLeaderboards/index.tsx
+++ src/pages/CommunityOpenRankLeaderboards/index.tsx
@@ -4,11 +4,15 @@ import styles from './styles.module.css';
 export default function CommunityOpenRankLeaderboards({ location }): JSX.Element {
   const currentPath = location.pathname;
   const pathes = currentPath.split('/');
-  const param = pathes[pathes.length - 1];
+  const org = pathes[pathes.length - 1];
+  let url = 'https://tyn1998.github.io/community-openrank-leaderboard/';
+  if (org !== 'community-openrank-leaderboards') {
+    url += `?organization=${org}`
+  }
   return (
     <div className='no-footer'>
       <Layout>
-        <iframe className={styles.mainRankingList} src='https://tyn1998.github.io/community-openrank-leaderboard/?organization=antgroup&project=antd&timeUnit=month&timeValue=1739635200000'></iframe>
+        <iframe className={styles.mainRankingList} src={url}></iframe>
       </Layout>
     </div>
   );
