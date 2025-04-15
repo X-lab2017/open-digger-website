306e9c04a0e43ab0ae279955039211e062fc7795
八鹤 <tangyenan.tyn@alibaba-inc.com>
2025-04-16 00:30:08 +0800

    fix: should consider pull preview path
    Signed-off-by: 八鹤 <tangyenan.tyn@alibaba-inc.com>

--- constants.ts
+++ constants.ts
@@ -1,2 +1,1 @@
-
 export const COMMUNITY_OPENRANK_LEADERBOARDS_URL_PATH = '/community-openrank-leaderboards';
--- src/pages/CommunityOpenRankLeaderboards/index.tsx
+++ src/pages/CommunityOpenRankLeaderboards/index.tsx
@@ -7,6 +7,7 @@ import { COMMUNITY_OPENRANK_LEADERBOARDS_URL_PATH } from '../../../constants';
 import styles from './styles.module.css';
 
 const PREFIX_PATH = '/community-openrank-leaderboard/';
+const END_POINT = 'https://tyn1998.github.io';
 
 function CommunityOpenRankLeaderboards({ location }): JSX.Element {
   const iframeRef = useRef<HTMLIFrameElement>(null);
@@ -17,7 +18,7 @@ function CommunityOpenRankLeaderboards({ location }): JSX.Element {
       if (event.data.type === 'COMMUNITY_OPENRANK_LEADERBOARD_ROUTE_CHANGE') {
         // route is something like "community-openrank-leaderboard?locale=zh&organization=xlab&project=open_digger&timeUnit=month&timeValue=1738393242809"
         // and we just need the "?locale..." part to construct the new url for the main window
-        const newPath = `${COMMUNITY_OPENRANK_LEADERBOARDS_URL_PATH}/${event.data.route.split(PREFIX_PATH)[1]}`;
+        const newPath = `${window.location.pathname.split(COMMUNITY_OPENRANK_LEADERBOARDS_URL_PATH)[0]}${COMMUNITY_OPENRANK_LEADERBOARDS_URL_PATH}/${event.data.route.split(PREFIX_PATH)[1]}`;
         window.history.replaceState(null, '', newPath);
         // tell the iframe current window's url since the iframe has no access to the main window.location
         iframeRef.current?.contentWindow?.postMessage({
@@ -31,7 +32,7 @@ function CommunityOpenRankLeaderboards({ location }): JSX.Element {
     };
   }, []);
 
-  const iframeUrl = `http://localhost:5173${PREFIX_PATH}${window.location.search}`;
+  const iframeUrl = `${END_POINT}${PREFIX_PATH}${window.location.search}`;
 
   return (
     <div className='no-footer'>
