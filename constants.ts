2b16947c21f893fa88d3591d17509a5bae05d150
八鹤 <tangyenan.tyn@alibaba-inc.com>
2025-04-15 23:55:47 +0800

    feat: communicate with iframe to support link share feature
    Signed-off-by: 八鹤 <tangyenan.tyn@alibaba-inc.com>

---
+++ constants.ts
@@ -0,0 +1,2 @@
+
+export const COMMUNITY_OPENRANK_LEADERBOARDS_URL_PATH = '/community-openrank-leaderboards';
--- src/pages/CommunityOpenRankLeaderboards/index.tsx
+++ src/pages/CommunityOpenRankLeaderboards/index.tsx
@@ -1,18 +1,41 @@
 import Layout from '@theme/Layout';
+import { useEffect, useRef } from 'react';
+
+import { COMMUNITY_OPENRANK_LEADERBOARDS_URL_PATH } from '../../../constants';
+
 import styles from './styles.module.css';
 
+const PREFIX_PATH = '/community-openrank-leaderboard/';
+
 export default function CommunityOpenRankLeaderboards({ location }): JSX.Element {
-  const currentPath = location.pathname;
-  const pathes = currentPath.split('/');
-  const org = pathes[pathes.length - 1];
-  let url = 'https://tyn1998.github.io/community-openrank-leaderboard/';
-  if (org !== 'community-openrank-leaderboards') {
-    url += `?organization=${org}`
-  }
+  const iframeRef = useRef<HTMLIFrameElement>(null);
+
+  // communicate with the iframe
+  useEffect(() => {
+    const handleIframeRouteChange = (event) => {
+      if (event.data.type === 'COMMUNITY_OPENRANK_LEADERBOARD_ROUTE_CHANGE') {
+        // route is something like "community-openrank-leaderboard?locale=zh&organization=xlab&project=open_digger&timeUnit=month&timeValue=1738393242809"
+        // and we just need the "?locale..." part to construct the new url for the main window
+        const newPath = `${COMMUNITY_OPENRANK_LEADERBOARDS_URL_PATH}/${event.data.route.split(PREFIX_PATH)[1]}`;
+        window.history.replaceState(null, '', newPath);
+        // tell the iframe current window's url since the iframe has no access to the main window.location
+        iframeRef.current?.contentWindow?.postMessage({
+          type: 'MAIN_WINDOW_URL_CHANGE', url: window.location.href 
+        }, '*');
+      }
+    };
+    window.addEventListener('message', handleIframeRouteChange, false);
+    return () => {
+      window.removeEventListener('message', handleIframeRouteChange);
+    };
+  }, []);
+
+  const iframeUrl = `http://localhost:5173${PREFIX_PATH}${window.location.search}`;
+
   return (
     <div className='no-footer'>
       <Layout>
-        <iframe className={styles.mainRankingList} src={url}></iframe>
+        <iframe ref={iframeRef} className={styles.mainRankingList} src={iframeUrl}></iframe>
       </Layout>
     </div>
   );
