3cad111882885f0c138ac8f1538dfa3171c8afc1
Frank Zhao <syzhao1988@126.com>
2025-10-14 14:19:00 +0800

    refactor: refactor leaderboards load logic (#118)
    Signed-off-by: frank-zsy <syzhao1988@126.com>

--- src/pages/leaderboards/index.tsx
+++ src/pages/leaderboards/index.tsx
@@ -19,57 +19,16 @@ function OpenLeaderboard() {
     if (!type) {
       type = 'projects';
     }
-    if (type === 'projects') {
-      axios.get(`https://oss.open-digger.cn/leaderboards/${type}.json`).then(resp => {
-        const data = resp.data;
-        setTitle(translate({ id: 'leaderboards.projects.title' }));
-        setData(data.data);
-        setOptions([
-          { name: '#', type: 'String', fields: ['rank'], width: 80 },
-          { name: translate({ id: 'leaderboards.projects.name' }), type: 'StringWithIcon', fields: ['name', 'logo'], width: 200 },
-          { name: 'OpenRank', type: 'String', fields: ['openrank'], width: 180 },
-          { name: translate({ id: 'leaderboards.projects.developers' }), type: 'String', fields: ['developerCount'], width: 180 },
-          { name: translate({ id: 'leaderboards.projects.initiator' }), type: 'StringWithIcon', fields: ['initiator', 'initiatorLogo'], width: 250 },
-          { name: translate({ id: 'leaderboards.projects.country' }), type: 'String', fields: ['country'], width: 150 },
-        ]);
-      }).catch(e => {
-        console.log(e);
-      });
-    } else if (type === 'companies') {
-      axios.get(`https://oss.open-digger.cn/leaderboards/${type}.json`).then(resp => {
-        const data = resp.data;
-        setTitle(translate({ id: 'leaderboards.companies.title' }));
-        setData(data.data);
-        setOptions([
-          { name: '#', type: 'String', fields: ['rank'], width: 80 },
-          { name: translate({ id: 'leaderboards.companies.name' }), type: 'StringWithIcon', fields: ['name', 'logo'], width: 300 },
-          { name: 'OpenRank', type: 'String', fields: ['openrank'], width: 200 },
-          { name: translate({ id: 'leaderboards.companies.repos' }), type: 'String', fields: ['repoCount'], width: 200 },
-          { name: translate({ id: 'leaderboards.companies.developers' }), type: 'String', fields: ['developerCount'], width: 200 },
-          { name: translate({ id: 'leaderboards.companies.country' }), type: 'String', fields: [i18n.currentLocale === 'zh' ? 'country_zh' : 'country'], width: 200 },
-        ]);
-      }).catch(e => {
-        console.log(e);
-      });
-    } else if (type === 'divisions') {
-      axios.get(`https://oss.open-digger.cn/leaderboards/${type}.json`).then(resp => {
-        const data = resp.data;
-        setTitle(translate({ id: 'leaderboards.provinces.title' }));
-        setData(data.data);
-        setOptions([
-          { name: '#', type: 'String', fields: ['rank'], width: 80 },
-          { name: translate({ id: 'leaderboards.provinces.name' }), type: 'String', fields: [i18n.currentLocale === 'zh' ? 'province_zh' : 'province'], width: 300 },
-          { name: 'OpenRank', type: 'String', fields: ['openrank'], width: 300 },
-          { name: translate({ id: 'leaderboards.provinces.developers' }), type: 'String', fields: ['developerCount'], width: 300 },
-          { name: translate({ id: 'leaderboards.provinces.country' }), type: 'String', fields: [i18n.currentLocale === 'zh' ? 'country_zh' : 'country'], width: 300 },
-        ]);
-      }).catch(e => {
-        console.log(e);
-      });
-    } else {
-      alert(`Leaderboard type ${type} is not supported.`);
-    }
 
+    axios.get(`https://oss.open-digger.cn/leaderboards/${type}.json`).then(resp => {
+      const data = resp.data;
+      setTitle(i18n.currentLocale === 'zh' ? data.title_zh : data.title);
+      setOptions(i18n.currentLocale === 'zh' ? data.options_zh : data.options);
+      setData(data.data);
+    }).catch(e => {
+      console.log(e);
+      setTitle(translate({ id: 'leaderboards.data_not_found' }, { type }));
+    });
   }, []);
 
   return (
